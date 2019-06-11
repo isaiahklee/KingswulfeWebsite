7bd07100b0c5d4c97d31d94430d3cb568bff8b4f
Isaiah Lee <isaiahklee0@gmail.com>
2019-06-10 23:57:27 -0500

    cleaned up code a bit

--- js/snek/snek-main.js
+++ js/snek/snek-main.js
@@ -21,8 +21,6 @@ function setup(){
     }
 
     //instantiate snake
-    //TODO
-    //board[0][0] = 1;
     snek = new snakebody(5, 5, boardsize);
 }
 
@@ -49,10 +47,6 @@ function draw(){
         ellipse(snek.bodynodes[i].x*(canvasH/boardsize)+20, snek.bodynodes[i].y*(canvasH/boardsize)+20, 25, 25);
     }
     
-    //draw mouse on screen and print its location to console
-    //let x2 = map(mouseX, 0, canvasW, 0, canvasW);
-    //let y2 = map(mouseY, 0, canvasH, 0, canvasH);
-    //ellipse(x2, y2, 5, 5);
     
 }
 
@@ -97,20 +91,16 @@ function dideat(){
     for(let i = 0; i < foodarray.length; i++){
         if(foodarray[i].x == snek.bodynodes[0].x && foodarray[i].y == snek.bodynodes[0].y){
             snek.add();
-            //console.log("et fud");
             delete foodarray[i];
         }
     }
 }
 
 function makefood(){
-    //console.log(foodarray);
-    //console.log(foodarray.length);
     //make this work if any elements are null?
     for(let i = 0; i < totalfood; i++){
         if(foodarray[i] == null){
                 foodarray[i] = new food(parseInt(Math.random()*boardsize), parseInt(Math.random()*boardsize));
-                //console.log(foodarray[i]);
         }
     }
     for(let i = 0; i < foodarray.length; i++){
